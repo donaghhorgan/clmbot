7c64e88a584c9efce053922d6834bb16fc6ef79f
Donagh Horgan <donagh.horgan@gmail.com>
2021-09-23 17:09:26 +0100

    Add gradio client + support for running models from local disk or HF Interference API

--- clmbot/deploy/client/__init__.py
+++ clmbot/deploy/client/__init__.py
@@ -1,6 +1,8 @@
 from .cli import CLIClient
+from .gradio import GradioClient
 from .client import Client
 
+gradio = Gradio = GradioClient
 cli = CLI = CLIClient
 
-__all__ = ["Client", "cli", "CLI", "CLIClient"]
+__all__ = ["Client", "cli", "CLI", "CLIClient", "gradio", "Gradio", "GradioClient"]
---
+++ clmbot/deploy/client/gradio.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass, field
+
+from .client import Client
+from typing import Dict, Any
+
+import gradio as gr
+import gradio.inputs
+import gradio.outputs
+
+
+@dataclass(frozen=True)
+class GradioClient(Client):
+    interface_args: Dict[str, Any] = field(default_factory=dict)
+    launch_args: Dict[str, Any] = field(default_factory=dict)
+
+    def __call__(self):
+        interface = gr.Interface(
+            fn=lambda prompt: self.generate(prompt),
+            inputs=[
+                gradio.inputs.Textbox(
+                    label="prompt",
+                    lines=10,
+                    placeholder="Write a prompt for the model to process",
+                )
+            ],
+            outputs=[
+                gradio.outputs.Textbox(
+                    label="response",
+                    type="str"
+                )
+            ],
+            **self.interface_args
+        )
+        interface.launch(**self.launch_args)
